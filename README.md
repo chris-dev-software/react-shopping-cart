90f56b86c081bfecf886a23e9ef77b5f0d81ba49
Christopher <chris.dev.software@gmail.com>
2024-03-31 21:49:42 -0500

    Terminando los filtros de categoria y precio

--- README.md
+++ README.md
@@ -7,8 +7,8 @@ https://fakestoreapi.com/products/categories
 Ecommerce
 
 - [x] Muestra una lista de productos que vienen de un JSON
-- [] Añade un filtro por categoría
-- [] Añade un filtro por precio
+- [x] Añade un filtro por categoría
+- [x] Añade un filtro por precio
 - [] Haz uso de useContext para evitar pasar props innecesarias.
 
 Carrito:
--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,19 @@ import { useEffect, useState } from 'react'
 
 function App () {
   const [products, setProducts] = useState([])
+  const [categories, setCategories] = useState([])
+  const [filters, setFilters] = useState({
+    minPrice: 0,
+    category: 'all'
+  })
+
+  const filterProducts = (products) => {
+    return products.filter(product => (
+      product.price >= filters.minPrice && (filters.category === 'all' || product.category === filters.category)
+    ))
+  }
+
+  const filteredProducts = filterProducts(products)
 
   useEffect(() => {
     const getProducts = async () => {
@@ -10,14 +23,61 @@ function App () {
       setProducts(data)
     }
 
+    const getCategories = async () => {
+      const response = await fetch('https://fakestoreapi.com/products/categories')
+      const data = await response.json()
+      setCategories(data)
+    }
+
     getProducts()
+    getCategories()
   }, [])
 
+  const handleChangeMinPrince = (event) => {
+    const newValue = Number(event.target.value)
+    setFilters(prevState => ({
+      ...prevState,
+      minPrice: newValue
+    }))
+  }
+
+  const handleChangeCategory = (event) => {
+    const newValue = event.target.value
+    setFilters(prevState => ({
+      ...prevState,
+      category: newValue
+    }))
+  }
+
   return (
-    <main>
-      <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10'>
-        {
-          products.map(product => (
+    <div className='max-w-7xl mx-auto px-10'>
+      <header>
+        <h1 className='text-center'>Tienda Online</h1>
+        <section className='flex items-center justify-between py-10'>
+          <div className='flex items-center gap-10'>
+            <label htmlFor='price'>Precio minimo:</label>
+            <input onChange={handleChangeMinPrince} type='range' id='price' min={0} max={1000} />
+            <span>S/. {filters.minPrice}</span>
+          </div>
+
+          <div className='flex items-center gap-10'>
+            <label htmlFor='category'>Categoria:</label>
+            <select onChange={handleChangeCategory} id='category'>
+              <option value='all'>Todos</option>
+              {
+                categories.map(category => (
+                  <option key={category} value={category}>{category}</option>
+                ))
+              }
+            </select>
+
+          </div>
+        </section>
+      </header>
+      <main>
+        <ul className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10'>
+          {
+          filteredProducts.map(product => (
             <li className='' key={product.id}>
               <img className='w-full aspect-square object-contain' src={product.image} alt={product.title} />
               <div className='p-5'>
@@ -30,8 +90,9 @@ function App () {
             </li>
           ))
         }
-      </ul>
-    </main>
+        </ul>
+      </main>
+    </div>
   )
 }
 
